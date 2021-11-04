7911d52ea5893ea764bfe12571b8694c0cfa7265
Georg Wolnik <92213086+gwolni2s@users.noreply.github.com>
2021-11-04 22:29:57 +0100

    update mit verbesserungen

--- Uebung05/5.2_Rednerliste.js
+++ Uebung05/5.2_Rednerliste.js
@@ -1,37 +1,60 @@
-document.getElementById("id3").addEventListener
-        ("click", addTalker);
+/**
+ * 1. start/stop does not work properly because time skips to the time without pause (sum does not work)
+ * 2. when speaker is added other timers will continue
+ */
 
-function addTalker() {
+document.getElementById("id3").addEventListener
+        ("click", addSpeaker);
+
+function addSpeaker() {
     //Create list item
     const listItem = document.createElement("li");
-    //Create/Get text node and append text
-    const node = document.createTextNode(document.getElementById("id1").value);
-    listItem.appendChild(node);
-    //Append time
-    listItem.appendChild(createTimer());
-    //Append button
-    listItem.appendChild(addButton());
-    //Create element and append element
-    const element = document.getElementById("id2");
-    element.appendChild(listItem);
-}
 
-function addButton() {
-    //Create, edit and append start button
+    //Create text node
+    const node = document.createTextNode(document.getElementById("id1").value);
+
+    //append node
+    listItem.appendChild(node);
+
+    //Create timer
+    const timer = document.createElement("a");
+
+    //Create now date
+    const now = new Date();
+    //edit timer
+    let myVar = setInterval(addTimer,1000);
+    function addTimer() {
+        const d = new Date();
+        const diff = d;
+        diff.setHours(d.getHours() - now.getHours());
+        diff.setMinutes(d.getMinutes() - now.getMinutes());
+        diff.setSeconds(d.getSeconds() - now.getSeconds());
+        timer.innerHTML = " " + diff.toLocaleTimeString() + " ";
+    }
+    //append timer
+    listItem.appendChild(timer);
+    //Create Button
     const button = document.createElement("button");
-    button.innerHTML = "Start";
+    //edit button
+    button.innerHTML = "Stop";
     button.type = "submit";
     button.addEventListener("click", function() {
         if(button.innerHTML === "Stop") {
             button.innerHTML = "Start";
+            clearInterval(myVar);
         } else {
             button.innerHTML = "Stop";
+            myVar = setInterval(addTimer, 1000);
         };
     });
-    return button;
-}
-function createTimer() {
-    const a =  document.createElement("a");
-    a.innerHTML = " " + new Date().toLocaleTimeString() + " ";
-    return a;
+
+    //Append Button
+    listItem.appendChild(button);
+
+    //Create element
+    const element = document.getElementById("id2");
+
+    //Append listItem
+    element.appendChild(listItem);
+
 }
