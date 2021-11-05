5e7f21b24c11c0aae3f8532a36f2d1303c875973
Georg Wolnik <92213086+gwolni2s@users.noreply.github.com>
2021-11-05 14:54:11 +0100

    update mit verbesserungen

--- Uebung05/5.2_Rednerliste.js
+++ Uebung05/5.2_Rednerliste.js
@@ -7,43 +7,61 @@ document.getElementById("id3").addEventListener
         ("click", addSpeaker);
 
 function addSpeaker() {
+
     //Create list item
     const listItem = document.createElement("li");
 
-    //Create text node
+    //Create text node with input name
     const node = document.createTextNode(document.getElementById("id1").value);
 
-    //append node
+    //append node with input name
     listItem.appendChild(node);
 
-    //Create timer
+    //Create timer element
     const timer = document.createElement("a");
 
-    //Create now date
-    const now = new Date();
-    //edit timer
+    //Create now date and diff
+    let now = new Date();
+    let diff;
+    //update of timer function every second
     let myVar = setInterval(addTimer,1000);
+
+    //function to create timer
     function addTimer() {
         const d = new Date();
-        const diff = d;
+        diff = d;
+
         diff.setHours(d.getHours() - now.getHours());
         diff.setMinutes(d.getMinutes() - now.getMinutes());
         diff.setSeconds(d.getSeconds() - now.getSeconds());
         timer.innerHTML = " " + diff.toLocaleTimeString() + " ";
     }
-    //append timer
+
+    //append timer to list
     listItem.appendChild(timer);
+
     //Create Button
     const button = document.createElement("button");
+
     //edit button
     button.innerHTML = "Stop";
     button.type = "submit";
+
+    /**
+     * when button shows start timer is does not run
+     * update of mytimer is cleared
+     *
+     * when button shows stop timer does run
+     * update od mytimer is set again
+     */
     button.addEventListener("click", function() {
         if(button.innerHTML === "Stop") {
             button.innerHTML = "Start";
             clearInterval(myVar);
         } else {
             button.innerHTML = "Stop";
+            //Set timer to 00:00:00
+            now = new Date();
             myVar = setInterval(addTimer, 1000);
         };
     });
@@ -54,7 +72,7 @@ function addSpeaker() {
     //Create element
     const element = document.getElementById("id2");
 
-    //Append listItem
+    //Append listItem to element
     element.appendChild(listItem);
 
 }
