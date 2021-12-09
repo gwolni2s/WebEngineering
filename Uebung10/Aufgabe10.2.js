2a2ac550c05b95eae8959b50dd9d7d022635fb6c
Georg Wolnik <92213086+gwolni2s@users.noreply.github.com>
2021-12-09 15:44:46 +0100

    Task 10.1 and 10.2

---
+++ Uebung10/Aufgabe10.2.js
@@ -0,0 +1,10 @@
+const add = (x,y) => x+y;
+const equals = (x,y) => x===y;
+
+console.log( add(1,2) );
+console.log( add(true, true ) );
+console.log( add(true, false ) );
+var y;
+var x = y = 1;
+console.log( add(equals(x,y), equals(y,x)) );
+
